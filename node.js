c34be9454963a581654f4de2335bc001d49961c4
prathameshpawar <72729956+prathameshpawar0333@users.noreply.github.com>
2022-12-08 09:17:55 +0530

    Update node.js

--- node.js
+++ node.js
@@ -1,4 +1,10 @@
-var hello = function(){  
-    print("Hello Nashorn");  
-};  
-hello();  
+import javax.script.*;  
+import java.io.*;  
+public class NashornExample {  
+    public static void main(String[] args) throws Exception{  
+        // Creating script engine  
+        ScriptEngine ee = new ScriptEngineManager().getEngineByName("Nashorn");  
+        // Reading Nashorn file  
+        ee.eval(new FileReader("js/hello.js"));  
+    }  
+}  
