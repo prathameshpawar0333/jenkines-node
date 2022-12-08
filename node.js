c96414b492db2e85f2a85436bc8a43f3178d7e6b
prathameshpawar <72729956+prathameshpawar0333@users.noreply.github.com>
2022-12-08 10:00:23 +0530

    commit 2

--- node.js
+++ node.js
@@ -2,4 +2,4 @@
 var java = require('java');
 var javaLangSystem = java.import('java.lang.System');
   
-javaLangSystem.out.printlnSync('hello sid sir!');
+javaLangSystem.out.printlnSync('hello sid sir! 1');
