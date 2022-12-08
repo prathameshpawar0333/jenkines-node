eeaf5174727227494d376da41bb50a13abd09938
prathameshpawar <72729956+prathameshpawar0333@users.noreply.github.com>
2022-12-08 09:20:35 +0530

    Update node.js

--- node.js
+++ node.js
@@ -1,10 +1,5 @@
-import javax.script.*;  
-import java.io.*;  
-public class NashornExample {  
-    public static void main(String[] args) throws Exception{  
-        // Creating script engine  
-        ScriptEngine ee = new ScriptEngineManager().getEngineByName("Nashorn");  
-        // Reading Nashorn file  
-        ee.eval(new FileReader("js/hello.js"));  
-    }  
-}  
+
+var java = require('java');
+var javaLangSystem = java.import('java.lang.System');
+  
+javaLangSystem.out.printlnSync('I love gfg!');
